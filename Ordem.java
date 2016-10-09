c74efc620e80d1515800187d7ab04d88c8d5ddc1
LaurenRolan <lauren_rolan@yahoo.com.br>
2016-10-08 21:06:06 -0300

    Update Ordem.java

--- Ordem.java
+++ Ordem.java
@@ -12,21 +12,58 @@ package musicaixa;
 public class Ordem extends ObjetoMusical {
     int BPM;
     String instrumento;
+    private int volume;
+
+    public Ordem(int BPM, String instrumento, int volume) {
+        this.BPM = BPM;
+        this.instrumento = instrumento;
+        this.volume = volume;
+    }
+       
+    public Ordem() {
+        this.BPM = 140;
+        this.instrumento = "Piano";
+        this.volume = 10;
+    }
 
     public int getBPM() {
         return BPM;
     }
-
-    public void setBPM(int BPM) {
-        this.BPM = BPM;
-    }
-
+    
     public String getInstrumento() {
         return instrumento;
     }
-
-    public void setInstrumento(String instrumento) {
-        this.instrumento = instrumento;
+    
+     public void trocaInstrumento() { // só um exemplo
+        if(this.instrumento == "Piano")
+        {
+            this.instrumento = "Guitar";
+        }        
     }
     
+    public void aumentaVolume()
+    {
+        this.volume = this.volume * 2;
+    }
+    
+    public void diminuiVolume()
+    {
+        this.volume = this.volume / 2;
+    }
+    
+    public void aumentaBPM()
+    {
+        if (this.BPM < 240)
+        {
+            this.BPM = this.BPM + 50;
+        }
+    }
+    
+    public void diminuiBPM()
+    {
+        if (this.BPM > 40)
+        {
+            this.BPM = this.BPM - 50;
+        }   
+    }
 }
