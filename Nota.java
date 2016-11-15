52e8f60b756f937881a394cc0ea5c77ae72768aa
LaurenRolan <lauren_rolan@yahoo.com.br>
2016-11-15 16:43:13 -0200

    Adicionando instrumento e protegendo os bagulhos

--- Nota.java
+++ Nota.java
@@ -12,53 +12,67 @@ package musicaixa;
 public class Nota extends ObjetoMusical {
     private char nota;
     private int oitava;
-    private int volume=12;
+    private int volume;
     private final int oitavaDefault = 3;
+    private final int volumeDefautl = 12;
     
     public Nota()
     {
         nota = 'C'; //começa em dó
         oitava = oitavaDefault;
+        volume = volumeDefault;
     }
     private String getVolume() {
         return String.format("a%d", volume);
     }
     private final int VOLUME_MAXIMO=120; //a definir
-    public void aumentaVolume()
+    protected void aumentaVolume()
     {
        if(volume<VOLUME_MAXIMO)
             volume+=VOLUME_MAXIMO*0.1;
     }
     
-    public void dobraVolume()
+    protected void dobraVolume()
     {
         volume *= 2;
         if(volume>VOLUME_MAXIMO)
             volume=VOLUME_MAXIMO;   //vamos saturar o volume?
     }
     @Override
-    public String toString(){
-        return String.format(" %c%d%s ", getNota(), getOitava(), getVolume());
+    protected String toString(){
+        return String.format(" %s %c%d%s ", getInstrumento(), getNota(), getOitava(), getVolume());
+    }
+    
+    protected void setInstrumento(int instrumento) {
+        this.instrumento = instrumento;
+    }
+    
+    private String getInstrumento() {
+        return String.format("I%d", this.instrumento);
+    }
+    
+    protected void somaInstrumento(int adicionado) {
+        if(instrumento + adicionado <= 128)
+            instrumento += adicionado;
     }
 
-
-    public char getNota() {
+    private char getNota() {
         return nota;
     }
 
-    public int getOitava() {
+    private int getOitava() {
         return oitava;
     }
     
     private final char PRIMEIRA_NOTA='A';
     private final char ULTIMA_NOTA='G';
     private final char PAUSA='R';
-    public void setNota(char nota) {
+    protected void setNota(char nota) {
         if((nota>=PRIMEIRA_NOTA && nota<=ULTIMA_NOTA)||nota==PAUSA)
             this.nota = nota;
     }
 
-    public void aumentaOitava() {
+    protected void aumentaOitava() {
         if (oitava < 8)
         {
             oitava = oitava + 1;
