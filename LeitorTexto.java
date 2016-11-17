ab87a8de00856cdea5f77f48e66628f5616a0062
cdflion <44637g@gmail.com>
2016-11-17 09:22:33 -0200

    leitor tem entrada File

--- LeitorTexto.java
+++ LeitorTexto.java
@@ -1,6 +1,7 @@
-package musicaixa;
+package som;
 
 import java.io.BufferedReader;
+import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
@@ -8,17 +9,15 @@ import java.io.IOException;
 
 public class LeitorTexto{
   private BufferedReader leitor_arquivo;
-  private String texto_lido;
-  
-  
+  private String texto_lido; 
   private final String NAO_ACHADO="Arquivo não encontrado!";
   private final String ERRO_LEITURA="Erro ao ler arquivo de texto!";
   private final String ERRO_FECHAR="Erro ao fechar arquivo!"; 
-  public String lerArquivo(String nomeArquivo){
+  public String lerArquivo(File arquivo){
       String texto_buffer;
       texto_lido="";
       try {
-          leitor_arquivo = new BufferedReader(new FileReader(nomeArquivo));
+          leitor_arquivo = new BufferedReader(new FileReader(arquivo));
           while((texto_buffer=leitor_arquivo.readLine())!=null){
               texto_lido+=texto_buffer;
           }
@@ -34,7 +33,5 @@ public class LeitorTexto{
       }
       return texto_lido;
   }
-  
-  
   //Passar o código leitor de texto pra cá
 }
